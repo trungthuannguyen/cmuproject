290a29d6369c37ac187586b97928f842a8027534
NGUYEN TRUNG THUAN <138069226+trungthuannguyen@users.noreply.github.com>
2023-07-31 22:35:59 -0400

    Update apppredict.py

--- housing_prediction/apppredict.py
+++ housing_prediction/apppredict.py
@@ -11,7 +11,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.impute import SimpleImputer
 
 # Header with logo
-logo_path = "team3vn_cmu.jpg"
+logo_path = "housing_prediction/team3vn_cmu.jpg"
 # Center the logo on the page
 col1, col2, col3, col4, col5, col6, col7, col8, col9 = st.columns(9)
 with col1:
@@ -171,7 +171,7 @@ def train_model(df):
     st.write("#### Model Performance")
     st.write("Mean Squared Error:", mean_squared_error(y_test, y_pred))
     st.write("R-squared Score:", r2_score(y_test, y_pred))
-    save_model(model, "LinearRegression.pkl")
+    save_model(model, "housing_prediction/LinearRegression.pkl")
     return model
 
 # Function to train and evaluate the Random Forest model
@@ -195,7 +195,7 @@ def train_model_random_forest(df):
     st.write("#### Model Random Forest Performance")
     st.write("Mean Squared Error:", mean_squared_error(y_test, y_pred))
     st.write("R-squared Score:", r2_score(y_test, y_pred))
-    save_model(model_rf, "RandomForest.pkl")
+    save_model(model_rf, "housing_prediction/RandomForest.pkl")
     return model_rf
 
 # Function to predict house prices using Linear Regression
