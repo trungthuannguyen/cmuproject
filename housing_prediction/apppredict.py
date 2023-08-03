05c44fb399201ca4b5b34d34149f9d2645d5da5a
NGUYEN TRUNG THUAN <138069226+trungthuannguyen@users.noreply.github.com>
2023-08-02 20:29:32 -0400

    Update apppredict.py

--- housing_prediction/apppredict.py
+++ housing_prediction/apppredict.py
@@ -4,13 +4,12 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler, MinMaxScaler  # Add MinMaxScaler
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import MinMaxScaler
+import pickle
 
 # Header with logo
 logo_path = "housing_prediction/team3vn_cmu.jpg"
@@ -157,7 +156,8 @@ def explore_data(df):
 
 # Function to save the trained model
 def save_model(model, filename):
-    st.cache_resource(model, filename)
+    with open(filename, 'wb') as file:
+        pickle.dump(model, file)
 
 # Function to train and evaluate the model
 def train_model(df):
